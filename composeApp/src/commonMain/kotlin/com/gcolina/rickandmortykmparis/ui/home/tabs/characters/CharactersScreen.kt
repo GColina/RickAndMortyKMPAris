674d01db0c64bc8da2ccf38f5476f5c1d786e820
GColina <gcolinaramirez@gmail.com>
2025-10-26 18:36:11 +0100

    Refactor: Cambio de tipo de dato en el ID del personaje
    Se modifica el tipo de dato del campo `id` de `String` a `Int` en los modelos `CharacterResponse` y `CharacterModel`.
    
    Además, se realizan los siguientes cambios:
    - Se añade el permiso de `INTERNET` en el `AndroidManifest.xml`.
    - Se implementa la pantalla `CharactersScreen` para observar el estado (`uiState`) desde el `CharactersViewModel`.
    - Se crea el Composable `CharacterOfTheDay` para mostrar la información de un personaje, incluyendo un indicador de carga.

--- composeApp/src/commonMain/kotlin/com/gcolina/rickandmortykmparis/ui/home/tabs/characters/CharactersScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gcolina/rickandmortykmparis/ui/home/tabs/characters/CharactersScreen.kt
@@ -4,18 +4,67 @@ package com.gcolina.rickandmortykmparis.ui.home.tabs.characters
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gcolina.rickandmortykmparis.domain.model.CharacterModel
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 
 @Composable
-fun CharactersScreen(){
-    val viewModel = koinViewModel<CharactersViewModel>()
+fun CharactersScreen() {
+    val charactersViewModel = koinViewModel<CharactersViewModel>()
+    val state by charactersViewModel.uiState.collectAsState()
 
 
 
-    Box(modifier = Modifier.fillMaxSize().background(Color.Red))
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = state.characterOfTheDay.toString())
+    }
+}
+
+@Composable
+fun CharacterOfTheDay(characterModel: CharacterModel? = null) {
+    Card(modifier = Modifier.fillMaxWidth().height(400.dp), shape = RoundedCornerShape(12)) {
+        if (characterModel == null) {
+            CircularProgressIndicator()
+        } else {
+            Box(contentAlignment = Alignment.BottomStart) {
+                Box(modifier = Modifier.fillMaxSize().background(Color.Green.copy(alpha = 0.5f)))
+                Box(
+                    modifier = Modifier.fillMaxSize().background(
+                        Brush.horizontalGradient(
+                            0f to Color.Black.copy(alpha = 0.9f),
+                            0.4f to Color.White.copy(alpha = 0f)
+                        )
+                    )
+                )
+                Text(
+                    text = characterModel.isAlive.toString(),
+                    fontSize = 40.sp,
+                    maxLines = 1,
+                    minLines = 1,
+                    color = Color.White,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+                )
+            }
+        }
+    }
 }
